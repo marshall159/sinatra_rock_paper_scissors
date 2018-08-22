e3bd1777157bd7f62a709ae7e27a223f618ea0bc
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-22 11:43:24 +0100

    add initial moves route

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
-get '/' do
-  'hello world'
+before do
+  @losing_moves = { :rock => :paper, :paper => :scissors, :scissors => :rock }
+  @moves = @losing_moves.keys
+end
+
+get '/:moves' do
+  "Your move was #{params[:moves]}"
 end
